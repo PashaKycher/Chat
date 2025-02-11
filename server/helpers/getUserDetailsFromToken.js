5f0672e24d7fcb13930fb6cd9e78e023044e210d
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-02-12 00:39:17 +0200

    drop token

--- server/helpers/getUserDetailsFromToken.js
+++ server/helpers/getUserDetailsFromToken.js
@@ -2,12 +2,12 @@ const UserModel = require("../models/userModel")
 const jwt = require("jsonwebtoken")
 
 const getUserDetailsFromToken = async(token) => {
-    if(!token){
-        return {
-            message : "Session out",
-            logout: true
-        }
-    }
+    // if(!token){
+    //     return {
+    //         message : "Session out",
+    //         logout: true
+    //     }
+    // }
 
     const decode = await jwt.verify(token, process.env.JWT_SECRET)
     const user = await UserModel.findById(token).select("-password")
